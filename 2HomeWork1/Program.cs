f17e522160bca2eee0cf817669465f93a47ca598
Roza Aitberova <rositka@yandex.ru>
2023-02-19 10:59:55 +0300

    1 задача второго семинара

---
+++ 2HomeWork1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number<1000 || number>100)
+{
+int lastDigit = number / 10;
+int firstDigit = lastDigit % 10;
+Console.WriteLine(firstDigit);
+}
+if (number>1000 || number <100)
+{
+    Console.WriteLine("Введено не трехзначное число");
+}
