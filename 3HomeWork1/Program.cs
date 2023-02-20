6048ebaaef49b983648fdd8b3697b2d2e22a67ae
Roza Aitberova <rositka@yandex.ru>
2023-02-20 20:34:54 +0300

    1 задача третьего семинара

---
+++ 3HomeWork1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите пятизначное число");
+string number = Console.ReadLine();
+int lengtA = number.Length;
+
+if (lengtA == 5)
+{
+    if (number[0] == number[4] && number [1] == number[3])
+    {
+        Console.WriteLine("палиндром");
+    }
+    else
+    {
+        Console.WriteLine("не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine ("число не является пятизначным"); 
+}
